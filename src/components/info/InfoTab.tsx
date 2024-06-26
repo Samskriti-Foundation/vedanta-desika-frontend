c7097ed39ff1d302888f49bbcc5b22ca5298302d
Pranav K Bhaskar <pkb19032002@gmail.com>
2024-06-26 10:55:22 +0530

    Added audio & video functionality

--- src/components/info/InfoTab.tsx
+++ src/components/info/InfoTab.tsx
@@ -1,5 +1,17 @@
 import React from "react"
-import { Text, Box } from "@chakra-ui/react"
+import {
+  Text,
+  Box,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Icon,
+} from "@chakra-ui/react"
+import { Clapperboard, Headphones, Type } from "lucide-react"
+
+import audioF from "../../assets/audio/goda_stuti_1.mp3"
 
 const textWithNewlines = (text: string) => {
   return (
@@ -29,8 +41,45 @@ export default function InfoTab({
       p={"4"}
       borderRadius={"md"}
       textAlign={"center"}
+      mx="auto"
+      my="4"
     >
-      {text === "" ? missing : textWithNewlines(text)}
+      <Tabs variant="soft-rounded" colorScheme="red" align="end">
+        <TabList>
+          <Tab>
+            <Icon as={Type} />
+          </Tab>
+          <Tab>
+            <Icon as={Headphones} />
+          </Tab>
+          <Tab>
+            <Icon as={Clapperboard} />
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel textAlign="center">
+            {text === "" ? missing : textWithNewlines(text)}
+          </TabPanel>
+          <TabPanel>
+            <Box display="flex" justifyContent="center">
+              <audio controls>
+                <source src={audioF} type="audio/mp3" />
+              </audio>
+            </Box>
+          </TabPanel>
+          <TabPanel>
+            <Box display="flex" justifyContent="center">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/KVdQ-VPiSbY"
+                title="Goda Stuti"
+                allowFullScreen
+              />
+            </Box>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Box>
   )
 }
